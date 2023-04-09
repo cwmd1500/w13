b5b089738e89655dfdaec34f9857f02e2e3c9375
cwmd1500 <71892925+cwmd1500@users.noreply.github.com>
2023-04-09 17:43:12 -0400

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+
+const myButton = document.querySelector('#myBtn');
+// Add a click event listener to the button element
+myButton.addEventListener('click', function() {
+
+  alert('Hello my class');
+  myButton.style.color = "red";
+})
